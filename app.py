cf07ee9071247fdc661e91655d960b607ad0a3b8
meanwhile <maxnext333@gmail.com>
2024-01-27 07:56:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,9 @@ for entityId in range(1,2048):
 print(f"[+] Find entitys {entitys}")
 print(f"[+]{readmapfrommem()}[+]")
 mapname = str(readmapfrommem()).strip('\x00')
-with open(f'maps/{mapname}/meta.json', 'r') as f:
-        data = json.load(f)
+file_path = os.path.join("maps", mapname, "meta.json")
+with open(file_path, 'r') as f:
+    data = json.load(f)
 scale = data['scale']
 x = data['offset']['x']
 y = data['offset']['y']
