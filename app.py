83857065429997f895fb22c1b1c82b228deab5c0
meanwhile <maxnext333@gmail.com>
2024-01-27 23:42:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,9 @@ def readmapfrommem():
     mapNameAddressbase = mapNameAddress_dll.base
     mapNameAddress = struct.unpack("<Q", cs2.memory.read(mapNameAddressbase + mapNameVal, 8, memprocfs.FLAG_NOCACHE))[0]
     mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
-    print('mapname raw', mapName)
-    return mapName
+    print('mapname raw', mapName, '/')
+    mapName.split(' ')
+    return mapName[0]
 
 def getentitys():
     entitys = []
