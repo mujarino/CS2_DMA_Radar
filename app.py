0cbb46dc9b769fcb85030b45ab07192392e49530
meanwhile <maxnext333@gmail.com>
2024-01-27 10:47:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,20 +50,9 @@ def readmapfrommem():
     mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0].decode()
     return mapName
 
-vmm = memprocfs.Vmm(['-device', 'fpga', '-disable-python', '-disable-symbols', '-disable-symbolserver', '-disable-yara', '-disable-yara-builtin', '-debug-pte-quality-threshold', '64'])
-cs2 = vmm.process('cs2.exe')
-client = cs2.module('client.dll')
-client_base = client.base
-print(f"[+] Client_base {client_base}")
-
-entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
-print(f"[+] Entitylist {entList}")
-
-player = struct.unpack("<Q", cs2.memory.read(client_base + dwLocalPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
-print(f"[+] Player {player}")
-
-entitys = []
-for entityId in range(1,2048):
+def getentitys():
+    entitys = []
+    for entityId in range(1,2048):
     EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
     try:
         entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
@@ -76,7 +65,22 @@ for entityId in range(1,2048):
             pass
     except:
         pass
-print(f"[+] Find entitys {entitys}")
+    return(entitys)
+
+
+vmm = memprocfs.Vmm(['-device', 'fpga', '-disable-python', '-disable-symbols', '-disable-symbolserver', '-disable-yara', '-disable-yara-builtin', '-debug-pte-quality-threshold', '64'])
+cs2 = vmm.process('cs2.exe')
+client = cs2.module('client.dll')
+client_base = client.base
+print(f"[+] Client_base {client_base}")
+
+entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+print(f"[+] Entitylist {entList}")
+
+player = struct.unpack("<Q", cs2.memory.read(client_base + dwLocalPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
+print(f"[+] Player {player}")
+
+
 mapname = str(readmapfrommem()).replace('\x00', '')
 mapname = mapname.replace('\x10\x0e', '')
 print(f"[+] Find map {mapname}")
@@ -95,53 +99,61 @@ pygame.display.set_caption("Mean Radar")
 radar_image = pygame.image.load(f'maps/{mapname}/radar.png')
 font = pygame.font.Font(None, hp_font_size)
 
-# Создание кнопок и меток
-running = True
-while running:
-    time_delta = clock.tick(60)/1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+try:
+    entitys = getentitys()
+    print(f"[+] Find entitys {entitys}")
+    running = True
+    while running:
+        time_delta = clock.tick(60)/1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
-    screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))
 
-    triangle_color = (255, 255, 255)
+        triangle_color = (255, 255, 255)
 
-    rotated_map_image, map_rect = pygame.transform.scale(radar_image, screen.get_size()), radar_image.get_rect()
-    screen.blit(rotated_map_image, map_rect.topleft)
+        rotated_map_image, map_rect = pygame.transform.scale(radar_image, screen.get_size()), radar_image.get_rect()
+        screen.blit(rotated_map_image, map_rect.topleft)
 
-    for entityId in entitys:
-        EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
-        entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
-        pX = struct.unpack("<f", cs2.memory.read(entity + m_vOldOrigin +0x4, 4, memprocfs.FLAG_NOCACHE))[0]
-        pY = struct.unpack("<f", cs2.memory.read(entity + m_vOldOrigin, 4, memprocfs.FLAG_NOCACHE))[0]
-        Hp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
-        team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
-        EyeAngles = struct.unpack("<fff", cs2.memory.read(entity +(m_angEyeAngles +0x4) , 12, memprocfs.FLAG_NOCACHE))
-        EyeAngles = math.radians(EyeAngles[0])
-        transformed_x, transformed_y = world_to_minimap(pX, pY, x, y, scale, radar_image, screen, zoom_scale)
-        triangle_top_x = transformed_x + math.sin(EyeAngles) * triangle_length
-        triangle_top_y = transformed_y + math.cos(EyeAngles) * triangle_length
-        triangle_left_x = transformed_x + math.sin(EyeAngles + math.pi / 3) * triangle_length / 2
-        triangle_left_y = transformed_y + math.cos(EyeAngles + math.pi / 3) * triangle_length / 2
-        triangle_right_x = transformed_x + math.sin(EyeAngles - math.pi / 3) * triangle_length / 2
-        triangle_right_y = transformed_y + math.cos(EyeAngles - math.pi / 3) * triangle_length / 2
-        if Hp > 0 and team == 2:
-            pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
-            pygame.draw.circle(screen, (255, 0, 0), (transformed_x, transformed_y), circle_size)
-        if Hp > 0 and team == 3:
-            pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
-            pygame.draw.circle(screen, (0, 0, 255), (transformed_x, transformed_y), circle_size)
-        if Hp>30:
-            text_surface = font.render(f'  {Hp}', True, (0, 255, 0))
-            text_surface.set_alpha(255)
-        if Hp<=30:
-            text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
-            text_surface.set_alpha(255)
-        if Hp==0:
-            text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
-            text_surface.set_alpha(0)
-        screen.blit(text_surface, (transformed_x, transformed_y))
-    pygame.display.flip()
+
+
+        for entityId in entitys:
+            EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
+            entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
+            pX = struct.unpack("<f", cs2.memory.read(entity + m_vOldOrigin +0x4, 4, memprocfs.FLAG_NOCACHE))[0]
+            pY = struct.unpack("<f", cs2.memory.read(entity + m_vOldOrigin, 4, memprocfs.FLAG_NOCACHE))[0]
+            Hp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+            team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
+            EyeAngles = struct.unpack("<fff", cs2.memory.read(entity +(m_angEyeAngles +0x4) , 12, memprocfs.FLAG_NOCACHE))
+            EyeAngles = math.radians(EyeAngles[0])
+            transformed_x, transformed_y = world_to_minimap(pX, pY, x, y, scale, radar_image, screen, zoom_scale)
+            triangle_top_x = transformed_x + math.sin(EyeAngles) * triangle_length
+            triangle_top_y = transformed_y + math.cos(EyeAngles) * triangle_length
+            triangle_left_x = transformed_x + math.sin(EyeAngles + math.pi / 3) * triangle_length / 2
+            triangle_left_y = transformed_y + math.cos(EyeAngles + math.pi / 3) * triangle_length / 2
+            triangle_right_x = transformed_x + math.sin(EyeAngles - math.pi / 3) * triangle_length / 2
+            triangle_right_y = transformed_y + math.cos(EyeAngles - math.pi / 3) * triangle_length / 2
+            if Hp > 0 and team == 2:
+                pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
+                pygame.draw.circle(screen, (255, 0, 0), (transformed_x, transformed_y), circle_size)
+            if Hp > 0 and team == 3:
+                pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
+                pygame.draw.circle(screen, (0, 0, 255), (transformed_x, transformed_y), circle_size)
+            if Hp>30:
+                text_surface = font.render(f'  {Hp}', True, (0, 255, 0))
+                text_surface.set_alpha(255)
+            if Hp<=30:
+                text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
+                text_surface.set_alpha(255)
+            if Hp==0:
+                text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
+                text_surface.set_alpha(0)
+            screen.blit(text_surface, (transformed_x, transformed_y))
+        pygame.display.flip()
+except:
+    print('error data reading. retrying in 5s')
+    time.pause(5)
+
 pygame.quit()
