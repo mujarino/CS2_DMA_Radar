d48d0e600610512045513de38fa223d73f0b0846
meanwhile <maxnext333@gmail.com>
2024-01-27 04:09:36 +0300

    Update enttest.py

--- enttest.py
+++ enttest.py
@@ -6,7 +6,7 @@ dwEntityList = 0x17CE6A0
 dwLocalPlayerPawn = 0x16D4F48
 m_iIDEntIndex = 0x1544
 m_iHealth = 0x32C
-
+m_angEyeAngles = 0x1518
 
 vmm = memprocfs.Vmm(['-device', 'fpga'])
 
@@ -27,11 +27,12 @@ print(f"[+] Player {player}")
 
 while True:
     entityId = struct.unpack("<I", cs2.memory.read(player + m_iIDEntIndex, 4, memprocfs.FLAG_NOCACHE))[0]
-    print(f"[+] entityId {entityId}")
     EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
     try:
         entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
         entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+        EyeAngles = struct.unpack("<Q", cs2.memory.read(entity +(m_angEyeAngles +0x4) , 8, memprocfs.FLAG_NOCACHE))[0]
+        print(f"[+] entityId {entityId} | EyeAngles {EyeAngles}")
     except:
         print("вы не смотрите на entity")
     time.sleep(0.1)
