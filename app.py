482aeced2bdd291af52847163985b35871ce6d33
meanwhile <maxnext333@gmail.com>
2024-01-27 23:31:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,9 @@ def readmapfrommem():
     mapNameAddress = struct.unpack("<Q", cs2.memory.read(mapNameAddressbase + mapNameVal, 8, memprocfs.FLAG_NOCACHE))[0]
     mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0]
     print('mapname raw', mapName)
-    mapName = mapName.split('\\\\')
-    print('mapname raw', mapName)
+    mapName = mapName.replace('\\','/')
+    print('mapname', mapName)
+    mapName = mapName.split('/')
     return mapName[0]
 
 def getentitys():
