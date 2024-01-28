3a9231b4e8d6b310b2701ed0327176dafa26a71e
meanwhile <maxnext333@gmail.com>
2024-01-28 19:18:21 +0300

    1

--- test.py
+++ test.py
@@ -1,74 +1,182 @@
 import memprocfs
 import struct
 import time
-import leechcorepyc as lc
+import pygame
+import pygame_gui
+import json
+import math
+import numpy as np
+import os
+import re
+
+########## ADJUST SIZES HERE ##########
+
+triangle_length = 13
+circle_size = 7 # 8 too big
+hp_font_size = 18
+rot_angle = 180
+#######################################
 
 dwEntityList = 0x17CE6A0
 dwLocalPlayerPawn = 0x16D4F48
-m_iIDEntIndex = 0x1544
 m_iHealth = 0x32C
-m_lifeState = 0x330
-m_iTeamNum = 0x3BF
-m_iszPlayerName = 0x640 # char[128]
-m_hPlayerPawn = 0x7EC
-m_iPawnArmor = 0x7FC
 m_vOldOrigin = 0x1224
+m_iTeamNum = 0x3BF
+m_angEyeAngles = 0x1518
+mapNameVal = 0x1CC200
+m_iCompTeammateColor = 0x738
 
-vmm = memprocfs.Vmm(['-device', 'fpga'])
+#######################################
+
+zoom_scale = 2
+
+def world_to_minimap(x, y, pos_x, pos_y, scale, map_image, screen, zoom_scale, rotation_angle):
+    image_x = int((x - pos_x) * screen.get_width() / (map_image.get_width() * scale * zoom_scale))
+    image_y = int((y - pos_y) * screen.get_height() / (map_image.get_height() * scale * zoom_scale))
+    center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
+    image_x, image_y = image_x - center_x, image_y - center_y
+    image_x, image_y = rotate_point((center_x, center_y), (image_x, image_y), rotation_angle)
+    return int(center_x + image_x), int(center_y + image_y)
+
+def rotate_point(center, point, angle):
+    angle_rad = math.radians(angle)
+    temp_point = point[0] - center[0], center[1] - point[1]
+    temp_point = (temp_point[0]*math.cos(angle_rad)-temp_point[1]*math.sin(angle_rad), temp_point[0]*math.sin(angle_rad)+temp_point[1]*math.cos(angle_rad))
+    temp_point = temp_point[0] + center[0], center[1] - temp_point[1]
+    return temp_point
 
 
+def getmapdata(mapname):
+    with open(f'maps/{mapname}/meta.json', 'r') as f:
+        data = json.load(f)
+    scale = data['scale']
+    x = data['offset']['x']
+    y = data['offset']['y']
+    return scale,x,y
+
+def readmapfrommem():
+    mapNameAddress_dll = cs2.module('matchmaking.dll')
+    mapNameAddressbase = mapNameAddress_dll.base
+    mapNameAddress = struct.unpack("<Q", cs2.memory.read(mapNameAddressbase + mapNameVal, 8, memprocfs.FLAG_NOCACHE))[0]
+    mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
+    return str(mapName)
+
+def getentitys():
+    entitys = []
+    for entityId in range(1,2048):
+        EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
+        try:
+            entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
+            entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+            team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
+            if int(team) == 1 or int(team) == 2 or int(team) == 3:
+                if entityHp>0 and entityHp<=100:
+                    entitys.append(entityId)
+            else:
+                pass
+        except:
+            pass
+    return(entitys)
+
+vmm = memprocfs.Vmm(['-device', 'fpga', '-disable-python', '-disable-symbols', '-disable-symbolserver', '-disable-yara', '-disable-yara-builtin', '-debug-pte-quality-threshold', '64'])
 cs2 = vmm.process('cs2.exe')
-
-
 client = cs2.module('client.dll')
 client_base = client.base
-print(f"[+] Client_base {client_base}")
-vmm.close()
+print(f"[+] Finded client base")
 
-device = "FPGA"
-leechcore = lc.LeechCore(device)
+entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+print(f"[+] Entered entitylist")
 
-data = leechcore.read(client_base + dwEntityList, 8)
-entList = struct.unpack("<Q", data)[0]
-print(f"[+] Entitylist {entList}")
+player = struct.unpack("<Q", cs2.memory.read(client_base + dwLocalPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
 
-player = struct.unpack("<Q", leechcore.read(client_base + dwLocalPlayerPawn, 8))[0]
-print(f"[+] Player {player}")
 
-def getinfo(index):
-    EntityENTRY = struct.unpack("<Q", leechcore.read((entList + 0x8 * (index >> 9) + 0x10), 8))[0]
-    entity = struct.unpack("<Q", leechcore.read(EntityENTRY + 120 * (index & 0x1FF), 8))[0]
+mapname = readmapfrommem()
 
-    Hp = struct.unpack("<I", leechcore.read(entity + m_iHealth, 4))[0]
-    Armor = struct.unpack("<I", leechcore.read(entity + m_iPawnArmor, 4))[0]
-    px = struct.unpack("<f", leechcore.read(entity + m_vOldOrigin +0x4, 4))[0]
-    py = struct.unpack("<f", leechcore.read(entity + m_vOldOrigin, 4))[0]
-    print(f"[+] ID - {index} | HP - {Hp} | X - {px} | Y - {px}")
+map_folders = [f for f in os.listdir('maps') if os.path.isdir(os.path.join('maps', f))]
 
-def infohandle(index):
-    EntityENTRY = struct.unpack("<Q", leechcore.read((entList + 0x8 * (index >> 9) + 0x10), 8))[0]
-    entity = struct.unpack("<Q", leechcore.read(EntityENTRY + 120 * (index & 0x1FF), 8))[0]
-    while True:
-        px = leechcore.read(entity + m_vOldOrigin +0x4, 4)
-        print(f"[+] X - {px}")
-        time.sleep(0.05)
+for folder in map_folders:
+    if folder in mapname:
+        mapname = folder
+        break
 
-entitys = []
-for entityId in range(1,700):
-    EntityENTRY = struct.unpack("<Q", leechcore.read((entList + 0x8 * (entityId >> 9) + 0x10), 8))[0]
+if mapname == 'empty':
+    print(f"[-] You are not connected to map")
+    exit()
+if os.path.exists(f'maps/{mapname}'):
+    pass
+else:
+    print(f'[-] Please, import this map first ({mapname})')
+    exit()
+print(f"[+] Found map {mapname}")
+scale,x,y = getmapdata(mapname)
+pygame.init()
+
+clock = pygame.time.Clock()
+screen_width, screen_height = 600, 600
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+pygame.display.set_caption("Mean Radar")
+map_image = pygame.image.load(f'maps/{mapname}/radar.png')
+font = pygame.font.Font(None, hp_font_size)
+
+while True:
     try:
-        entity = struct.unpack("<Q", leechcore.read(EntityENTRY + 120 * (entityId & 0x1FF), 8))[0]
-        entityHp = struct.unpack("<I", leechcore.read(entity + m_iHealth, 4))[0]
-        if int(entityHp) != 0:
-            entitys.append(entityId)
-        else:
-            pass
+        entitys = getentitys()
+        print(f"[+] Find {len(entitys)} entitys")
+        try:
+            entitys[0]
+        except:
+            0/0
+        running = True
+        while running:
+            time_delta = clock.tick(60)/1000.0
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+
+            screen.fill((0, 0, 0))
+
+            triangle_color = (255, 255, 255)
+
+            rotated_map_image, map_rect = rotate_image(pygame.transform.scale(map_image, screen.get_size()), player_view_angle_y)
+            screen.blit(rotated_map_image, map_rect.topleft)
+
+
+
+            for entityId in entitys:
+                EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
+                entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
+                pX = struct.unpack("<f", cs2.memory.read(entity + m_vOldOrigin +0x4, 4, memprocfs.FLAG_NOCACHE))[0]
+                pY = struct.unpack("<f", cs2.memory.read(entity + m_vOldOrigin, 4, memprocfs.FLAG_NOCACHE))[0]
+                Hp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+                team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
+                EyeAngles = struct.unpack("<fff", cs2.memory.read(entity +(m_angEyeAngles +0x4) , 12, memprocfs.FLAG_NOCACHE))
+                EyeAngles = math.radians(EyeAngles[0])
+                transformed_x, transformed_y = world_to_minimap(pX, pY, x, y, scale, map_image, screen, zoom_scale, rot_angle)
+                triangle_top_x = transformed_x + math.sin(EyeAngles) * triangle_length
+                triangle_top_y = transformed_y + math.cos(EyeAngles) * triangle_length
+                triangle_left_x = transformed_x + math.sin(EyeAngles + math.pi / 3) * triangle_length / 2
+                triangle_left_y = transformed_y + math.cos(EyeAngles + math.pi / 3) * triangle_length / 2
+                triangle_right_x = transformed_x + math.sin(EyeAngles - math.pi / 3) * triangle_length / 2
+                triangle_right_y = transformed_y + math.cos(EyeAngles - math.pi / 3) * triangle_length / 2
+                if Hp > 0 and team == 2:
+                    pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
+                    pygame.draw.circle(screen, (255, 0, 0), (transformed_x, transformed_y), circle_size)
+                if Hp > 0 and team == 3:
+                    pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
+                    pygame.draw.circle(screen, (0, 0, 255), (transformed_x, transformed_y), circle_size)
+                if Hp>30:
+                    text_surface = font.render(f'  {Hp}', True, (0, 255, 0))
+                    text_surface.set_alpha(255)
+                if Hp<=30:
+                    text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
+                    text_surface.set_alpha(255)
+                if Hp==0:
+                    text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
+                    text_surface.set_alpha(0)
+                screen.blit(text_surface, (transformed_x, transformed_y))
+            pygame.display.flip()
     except:
-        pass
-
-print(entitys)
-target = entitys[0]
-infohandle(target)
-#for index in entitys:
-    #getinfo(index)
-
+        print('[-] Error data reading. Some entity leave or map closed. Closing program')
+        exit()
+pygame.quit()
