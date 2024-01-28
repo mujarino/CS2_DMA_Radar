7d62bd49b672b9b0078a63ab246a53cde73184a7
meanwhile <maxnext333@gmail.com>
2024-01-28 19:32:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -34,9 +34,8 @@ def world_to_minimap(x, y, pos_x, pos_y, scale, map_image, screen, zoom_scale, r
     image_x = int((x - pos_x) * screen.get_width() / (map_image.get_width() * scale * zoom_scale))
     image_y = int((y - pos_y) * screen.get_height() / (map_image.get_height() * scale * zoom_scale))
     center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
-    image_x, image_y = image_x - center_x, image_y - center_y
     image_x, image_y = rotate_point((center_x, center_y), (image_x, image_y), rotation_angle)
-    return int(center_x + image_x), int(center_y + image_y)
+    return int(image_x), int(image_y)
 
 def rotate_point(center, point, angle):
     angle_rad = math.radians(angle)
