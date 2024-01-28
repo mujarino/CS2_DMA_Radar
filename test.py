4a9a3364edccd3eb11d33afa06ad5d38424d46f1
meanwhile <maxnext333@gmail.com>
2024-01-28 20:20:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -151,7 +151,6 @@ while True:
 
             rotated_map_image, map_rect = rotate_image(pygame.transform.scale(map_image, screen.get_size()), rot_angle)
             screen.blit(rotated_map_image, map_rect.topleft)
-            rot_plus_button.set_position([50, 50])
 
 
 
@@ -188,6 +187,7 @@ while True:
                     text_surface = font.render(f'  {Hp}', True, (255, 0, 0))
                     text_surface.set_alpha(0)
                 screen.blit(text_surface, (transformed_x, transformed_y))
+                rot_plus_button.set_position([50, 50])
             pygame.display.flip()
     #except:
         #print('[-] Error data reading. Some entity leave or map closed. Closing program')
