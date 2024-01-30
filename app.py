0f6abe517799b63a6cf757d5e9debe26c902b65c
meanwhile <maxnext333@gmail.com>
2024-01-31 01:07:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import math
 import numpy as np
 import os
 import re
-import asyncio
 
 ########## ADJUST SIZES HERE ##########
 
@@ -76,21 +75,21 @@ def rotate_image(image, angle):
     new_rect = rotated_image.get_rect(center = image.get_rect().center)
     return rotated_image, new_rect
 
-async def getentitys():
+def getentitys():
     entitys = []
     for entityId in range(1,2048):
-        EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))
+        EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
         try:
-            entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))
-            entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))
-            team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))
+            entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
+            entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+            team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
             if int(team) == 1 or int(team) == 2 or int(team) == 3:
                 if entityHp>0 and entityHp<=100:
-                    EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))
-                    entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))
+                    EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
+                    entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
                     entitys.append(entity)
-                else:
-                    pass
+            else:
+                pass
         except:
             pass
     return(entitys)
@@ -137,77 +136,67 @@ class player1:
             text_surface.set_alpha(0)
         screen.blit(text_surface, (transformed_x, transformed_y))
 
-async def main():
-    triangle_length = 13
-    circle_size = 7 # 8 too big
-    hp_font_size = 18
-    rot_angle = 0
-    maps_with_split = ['de_nuke','de_vertigo']
-    dwEntityList = 0x17CE6A0
-    dwLocalPlayerPawn = 0x16D4F48
-    m_iHealth = 0x32C
-    m_vOldOrigin = 0x1224
-    m_iTeamNum = 0x3BF
-    m_angEyeAngles = 0x1518
-    mapNameVal = 0x1CC200
-    m_iCompTeammateColor = 0x738
-    m_bIsDefusing = 0x13B0
+vmm = memprocfs.Vmm(['-device', 'fpga', '-disable-python', '-disable-symbols', '-disable-symbolserver', '-disable-yara', '-disable-yara-builtin', '-debug-pte-quality-threshold', '64'])
+cs2 = vmm.process('cs2.exe')
+client = cs2.module('client.dll')
+client_base = client.base
+print(f"[+] Finded client base")
 
-    #######################################
-    zoom_scale = 2
-    vmm = memprocfs.Vmm(['-device', 'fpga', '-disable-python', '-disable-symbols', '-disable-symbolserver', '-disable-yara', '-disable-yara-builtin', '-debug-pte-quality-threshold', '64'])
-    cs2 = vmm.process('cs2.exe')
-    client = cs2.module('client.dll')
-    client_base = client.base
-    print(f"[+] Finded client base")
+entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+print(f"[+] Entered entitylist")
 
-    entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
-    print(f"[+] Entered entitylist")
+player = struct.unpack("<Q", cs2.memory.read(client_base + dwLocalPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
 
-    player = struct.unpack("<Q", cs2.memory.read(client_base + dwLocalPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
+mapname = readmapfrommem()
 
-    mapname = readmapfrommem()
+map_folders = [f for f in os.listdir('maps') if os.path.isdir(os.path.join('maps', f))]
 
-    map_folders = [f for f in os.listdir('maps') if os.path.isdir(os.path.join('maps', f))]
+for folder in map_folders:
+    if folder in mapname:
+        mapname = folder
+        break
 
-    for folder in map_folders:
-        if folder in mapname:
-            mapname = folder
-            break
+if mapname == 'empty':
+    print(f"[-] You are not connected to map")
+    exit()
+if os.path.exists(f'maps/{mapname}'):
+    pass
+else:
+    print(f'[-] Please, import this map first ({mapname})')
+    exit()
+print(f"[+] Found map {mapname}")
+if mapname in maps_with_split:
+    lowerx,lowery,lowerz = getlowermapdata(mapname)
+scale,x,y = getmapdata(mapname)
+pygame.init()
 
-    if mapname == 'empty':
-        print(f"[-] You are not connected to map")
-        exit()
-    if os.path.exists(f'maps/{mapname}'):
-        pass
-    else:
-        print(f'[-] Please, import this map first ({mapname})')
-        exit()
-    print(f"[+] Found map {mapname}")
-    if mapname in maps_with_split:
-        lowerx,lowery,lowerz = getlowermapdata(mapname)
-    scale,x,y = getmapdata(mapname)
-    pygame.init()
+manager = pygame_gui.UIManager((600, 600))
+clock = pygame.time.Clock()
+screen_width, screen_height = 600, 600
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+pygame.display.set_caption("CS2 Radar")
+map_image = pygame.image.load(f'maps/{mapname}/radar.png')
+font = pygame.font.Font(None, hp_font_size)
+rot_plus_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 50), (120, 30)), text='ANGLE+90', manager=manager)
 
-    manager = pygame_gui.UIManager((600, 600))
-    clock = pygame.time.Clock()
-    screen_width, screen_height = 600, 600
-    screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
-    pygame.display.set_caption("CS2 Radar")
-    map_image = pygame.image.load(f'maps/{mapname}/radar.png')
-    font = pygame.font.Font(None, hp_font_size)
-    rot_plus_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 50), (120, 30)), text='ANGLE+90', manager=manager)
-    while True:
-        entitys = await getentitys()
-        players = []
-        for entity in entitys:
-            p = player1(entity)
-            players.append(p)
-        print(f"[+] Find {len(entitys)} entitys")
+while True:
+    running = True
+    while running:
         try:
-            entitys
+            entitys = getentitys()
+            players = []
+            for entity in entitys:
+                p = player1(entity)
+                players.append(p)
+            print(f"[+] Find {len(entitys)} entitys")
+            try:
+                entitys[0]
+            except:
+                0/0
         except:
-            0/0
+            print('[-] Error data reading. Some entity leave or map closed. Closing program')
+            exit()
+
         time_delta = clock.tick(60)/1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -218,18 +207,18 @@ async def main():
                     if event.ui_element == rot_plus_button:
                         rot_angle += 90
         manager.update(time_delta)
+
         screen.fill((0, 0, 0))
+
         triangle_color = (255, 255, 255)
+
         rotated_map_image, map_rect = rotate_image(pygame.transform.scale(map_image, screen.get_size()), rot_angle)
         rot_plus_button.set_position([50, 50])
         screen.blit(rotated_map_image, map_rect.topleft)
         manager.draw_ui(screen)
         for p in players:
             p.draw(screen)
+
         pygame.display.flip()
-
-if __name__ == '__main__':
-    asyncio.run(main())
-
 pygame.quit()
 
