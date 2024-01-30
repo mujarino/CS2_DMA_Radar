ade5fce8739bd2f5d8385984c53646f83c7bdaef
meanwhile <maxnext333@gmail.com>
2024-01-30 20:59:45 +0300

    Update enttest.py

--- testing/enttest.py
+++ testing/enttest.py
@@ -43,6 +43,7 @@ def getentitys():
             entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
             entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
             team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
+            print(team)
             if int(team) == 1 or int(team) == 2 or int(team) == 3:
                 EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
                 entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
