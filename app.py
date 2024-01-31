a3188afc83783726ea33aa83c761608335da7067
meanwhile <maxnext333@gmail.com>
2024-01-31 05:58:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -82,12 +82,8 @@ def getentitys():
         try:
             entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
             entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
-            team = struct.unpack("<I", cs2.memory.read(entity + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
-            if int(team) == 1 or int(team) == 2 or int(team) == 3:
-                if entityHp>0 and entityHp<=100:
-                    EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
-                    entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
-                    entitys.append(entity)
+            if entityHp>0 and entityHp<=100:
+                entitys.append(entity)
             else:
                 pass
         except:
