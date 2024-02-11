33acff369d0bdda254b352d8d8b4ff4db2f95299
meanwhile <maxnext333@gmail.com>
2024-02-11 17:06:08 +0300

    Create mapnameparser.py

---
+++ testing/mapnameparser.py
@@ -0,0 +1,41 @@
+import memprocfs
+import struct
+import time
+import pygame
+import pygame_gui
+import json
+import math
+import numpy as np
+import os
+import re
+
+dwEntityList = 0x18B0FC8 # offsets.py
+dwLocalPlayerPawn = 0x17262E8 #offsets.py
+m_iPawnHealth = 0x7F0
+m_iPawnArmor = 0x7F4
+m_bPawnIsAlive = 0x7EC
+m_angEyeAngles = 0x1578
+m_iTeamNum = 0x3CB
+m_hPlayerPawn = 0x7E4
+m_vOldOrigin = 0x127C
+m_iIDEntIndex = 0x15A4
+m_iHealth = 0x334
+
+vmm = memprocfs.Vmm(['-device', 'fpga'])
+
+
+cs2 = vmm.process('cs2.exe')
+
+mapNameAddress_dll = cs2.module('matchmaking.dll')
+mapNameAddressbase = mapNameAddress_dll.base
+
+for mapNameVal in range(1,5000):
+    try:
+        mapNameAddress = struct.unpack("<Q", cs2.memory.read(mapNameAddressbase + mapNameVal, 8, memprocfs.FLAG_NOCACHE))[0]
+        mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
+        if 'de_mirage' in mapName:
+            print(mapNameVal)
+    except:
+        pass
+
+
