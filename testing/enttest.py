89ef3278a22a85dc790f4f2357b09e12710a3ccc
meanwhile <maxnext333@gmail.com>
2024-02-11 16:40:10 +0300

    Update enttest.py

--- testing/enttest.py
+++ testing/enttest.py
@@ -9,15 +9,15 @@ import numpy as np
 import os
 import re
 
-dwEntityList = 0x18AFFB8 # offsets.py
-dwLocalPlayerPawn = 0x17252B8 #offsets.py
-m_iHealth = 0x14F8  #client.dll.py
-m_vOldOrigin = 0x127C #client.dll.py
-m_iTeamNum = 0x3CB #client.dll.py
-m_angEyeAngles = 0x1578 #client.dll.py
-mapNameVal = 0x1CC200 #I don't know where you found it
-m_iCompTeammateColor = 0x7300 #client.dll.py
-m_bIsDefusing = 0x1408 #client.dll.py
+dwEntityList = 0x18B0FC8 # offsets.py
+dwLocalPlayerPawn = 0x17262E8 #offsets.py
+m_iPawnHealth = 0x7F0
+m_iPawnArmor = 0x7F4
+m_bPawnIsAlive = 0x7EC
+m_angEyeAngles = 0x1578
+m_iTeamNum = 0x3CB
+m_hPlayerPawn = 0x7E4
+m_vOldOrigin = 0x127C
 
 vmm = memprocfs.Vmm(['-device', 'fpga'])
 
@@ -29,26 +29,31 @@ client = cs2.module('client.dll')
 client_base = client.base
 print(f"[+] Client_base {client_base}")
 
-entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+EntityList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+EntityList = struct.unpack("<Q", cs2.memory.read(EntityList + 0x10, 8, memprocfs.FLAG_NOCACHE))[0]
 print(f"[+] Entered entitylist")
+
+EntityPawnListEntry = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+
+
 
 def getentitys():
     entitys = []
-    for entityId in range(1,2048):
-        EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
+    for index in range(1,64):
         try:
-            entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
-            entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
-            if entityHp>0 and entityHp<=100:
-                print(entityHp)
-                entitys.append(entity)
-            else:
+            EntityAddress = struct.unpack("<Q", cs2.memory.read(EntityList + (index + 1) * 0x78, 8, memprocfs.FLAG_NOCACHE))[0]
+            Pawn = struct.unpack("<Q", cs2.memory.read(EntityAddress + m_hPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
+            EntityPawnListEntry = struct.unpack("<Q", cs2.memory.read(EntityPawnListEntry + 0x10 + 8 * (Pawn & 0x7FFF) >> 9, 8, memprocfs.FLAG_NOCACHE))[0]
+            Pawn = struct.unpack("<Q", cs2.memory.read(EntityPawnListEntry + 0x78 * (Pawn & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
+                entityHp = struct.unpack("<I", cs2.memory.read(EntityAddress + m_iPawnHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+                if entityHp>0 and entityHp<=100:
+                    print(entityHp)
+                    entitys.append(entity)
+                else:
+                    pass
+            except:
                 pass
-        except:
-            pass
     return(entitys)
 
 print(len(getentitys()))
-
-
 
