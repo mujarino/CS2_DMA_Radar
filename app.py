a4be0685690e67e0773f9f97bba2a095d79ca7b9
meanwhile <maxnext333@gmail.com>
2024-02-11 17:42:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,10 +69,7 @@ def getlowermapdata(mapname):
     return lowerx,lowery,z
 
 def readmapfrommem():
-    mapNameAddress_dll = cs2.module('matchmaking.dll')
-    mapNameAddressbase = mapNameAddress_dll.base
-    mapNameAddress = struct.unpack("<Q", cs2.memory.read(mapNameAddressbase + mapNameVal, 8, memprocfs.FLAG_NOCACHE))[0]
-    mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
+    mapName = struct.unpack("<32s", cs2.memory.read(client_base + mapNameVal, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
     return str(mapName)
 
 def rotate_image(image, angle):
