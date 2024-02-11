5e367e9b11ad86d11f0c05121af97bb0d780c925
meanwhile <maxnext333@gmail.com>
2024-02-11 18:08:20 +0300

    testing mapread

--- testing/mapnameparser.py
+++ testing/mapnameparser.py
@@ -20,13 +20,13 @@ m_hPlayerPawn = 0x7E4
 m_vOldOrigin = 0x127C
 m_iIDEntIndex = 0x15A4
 m_iHealth = 0x334
-mapNameVal = 0x19E60E4
+mapNameVal = 0x13C04C0
 
 vmm = memprocfs.Vmm(['-device', 'fpga'])
 
 cs2 = vmm.process('cs2.exe')
 
-mapNameAddress_dll = cs2.module('client.dll')
+mapNameAddress_dll = cs2.module('server.dll')
 mapNameAddressbase = mapNameAddress_dll.base
 mapName = struct.unpack("<32s", cs2.memory.read(mapNameAddressbase + mapNameVal, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
 print(mapName)
