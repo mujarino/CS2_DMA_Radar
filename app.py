b0cde5f5654fb7e6038124795ec35c4fba1bd08d
meanwhile <maxnext333@gmail.com>
2024-02-23 16:31:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -96,9 +96,8 @@ def getentitypawns():
     entitys = []
     EntityList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
     EntityList = struct.unpack("<Q", cs2.memory.read(EntityList + 0x10, 8, memprocfs.FLAG_NOCACHE))[0]
-    for i in range(1,64):
+    for i in range(0,65):
         try:
-
             EntityAddress = struct.unpack("<Q", cs2.memory.read(EntityList + (i + 1) * 0x78, 8, memprocfs.FLAG_NOCACHE))[0]
             EntityPawnListEntry = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
             Pawn = struct.unpack("<Q", cs2.memory.read(EntityAddress + m_hPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
@@ -106,7 +105,7 @@ def getentitypawns():
             Pawn = struct.unpack("<Q", cs2.memory.read(EntityPawnListEntry + 0x78 * (Pawn & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
             entitys.append(Pawn)
         except:
-            pass
+            return
     return(entitys)
 
 class player1:
