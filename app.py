ead3697d0569c1fcc48781af3e96a3df417681f7
meanwhile <maxnext333@gmail.com>
2024-02-23 20:13:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,11 +53,14 @@ map_folders = [f for f in os.listdir('maps') if os.path.isdir(os.path.join('maps
 research = 0
 
 def world_to_minimap(x, y, pos_x, pos_y, scale, map_image, screen, zoom_scale, rotation_angle):
-    image_x = int((x - pos_x) * screen.get_width() / (map_image.get_width() * scale * zoom_scale))
-    image_y = int((y - pos_y) * screen.get_height() / (map_image.get_height() * scale * zoom_scale))
-    center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
-    image_x, image_y = rotate_point((center_x, center_y), (image_x, image_y), rotation_angle)
-    return int(image_x), int(image_y)
+    try:
+        image_x = int((x - pos_x) * screen.get_width() / (map_image.get_width() * scale * zoom_scale))
+        image_y = int((y - pos_y) * screen.get_height() / (map_image.get_height() * scale * zoom_scale))
+        center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
+        image_x, image_y = rotate_point((center_x, center_y), (image_x, image_y), rotation_angle)
+        return int(image_x), int(image_y)
+    except:
+        return 0,0
 
 def rotate_point(center, point, angle):
     angle_rad = math.radians(angle)
