b595a1251ac804ec5d3129a644b6ce782dad6d45
meanwhile <maxnext333@gmail.com>
2024-02-23 13:56:48 +0300

    Update enttest.py

--- testing/enttest.py
+++ testing/enttest.py
@@ -30,24 +30,9 @@ m_vOldOrigin = clientdll['C_BasePlayerPawn']['data']['m_vOldOrigin']['value']
 m_iIDEntIndex = clientdll['C_CSPlayerPawnBase']['data']['m_iIDEntIndex']['value']
 m_iHealth = clientdll['C_BaseEntity']['data']['m_iHealth']['value']
 mapNameVal = offsets['matchmaking_dll']['data']['dwGameTypes_mapName']['value']
-m_bIsDefusing = 5128
-hasdefuser = 5540
+
 
 print('[+] offsets parsed')
-def getentitys():
-    entitys = []
-    for entityId in range(1,2048):
-        EntityENTRY = struct.unpack("<Q", cs2.memory.read((entList + 0x8 * (entityId >> 9) + 0x10), 8, memprocfs.FLAG_NOCACHE))[0]
-        try:
-            entity = struct.unpack("<Q", cs2.memory.read(EntityENTRY + 120 * (entityId & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
-            entityHp = struct.unpack("<I", cs2.memory.read(entity + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
-            if entityHp>0 and entityHp<=100:
-                entitys.append(entity)
-            else:
-                pass
-        except:
-            pass
-    return(entitys)
 
 
 vmm = memprocfs.Vmm(['-device', 'fpga'])
@@ -59,18 +44,15 @@ cs2 = vmm.process('cs2.exe')
 client = cs2.module('client.dll')
 client_base = client.base
 print(f"[+] Client_base {client_base}")
-entList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+EntityList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+EntityList = struct.unpack("<Q", cs2.memory.read(EntityList + 0x10, 8, memprocfs.FLAG_NOCACHE))[0]
+for i in range(1,64):
 
-enitys = getentitys()
+    EntityAddress = struct.unpack("<Q", cs2.memory.read(EntityList + (index + 1) * 0x78, 8, memprocfs.FLAG_NOCACHE))[0]
+    EntityPawnListEntry = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+    Pawn = struct.unpack("<Q", cs2.memory.read(EntityAddress + m_hPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
+    EntityPawnListEntry = struct.unpack("<Q", cs2.memory.read(EntityPawnListEntry + 0x10 + 8 * ((Pawn & 0x7FFF) >> 9), 8, memprocfs.FLAG_NOCACHE))[0]
+    Pawn = struct.unpack("<Q", cs2.memory.read(EntityPawnListEntry + 0x78 * (Pawn & 0x1FF), 8, memprocfs.FLAG_NOCACHE))[0]
+    print(struct.unpack("<I", cs2.memory.read(EntityAddress + m_iPawnHealth, 4, memprocfs.FLAG_NOCACHE))[0])
 
-while True:
-    defuses = []
-    for entity_pawn in enitys:
-        try:
-            IsDefusing = struct.unpack("<I", cs2.memory.read(entity_pawn + hasdefuser, 4, memprocfs.FLAG_NOCACHE))
-            defuses.append(IsDefusing[0])
-        except:
-            print('some entity is defusing!!')
-            pass
-    print(defuses)
-    time.sleep(1)
+
