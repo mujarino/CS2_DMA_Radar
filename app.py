773cdac7fd56bfeff951c8fb1aeaa9d101648868
meanwhile <maxnext333@gmail.com>
2024-02-24 00:52:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,7 @@ def get_only_mapname():
 def pawnhandler():
     global global_entity_list
     global playerTeam
+    global mappp
     while True:
         try:
             entityss = getentitypawns()
@@ -117,6 +118,11 @@ def pawnhandler():
             
             player = struct.unpack("<Q", cs2.memory.read(client_base + dwLocalPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
             playerTeam = struct.unpack("<I", cs2.memory.read(player + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
+            mapNameAddress = struct.unpack("<Q", cs2.memory.read(mapNameAddressbase + mapNameVal, 8, memprocfs.FLAG_NOCACHE))[0]
+            mapname = struct.unpack("<32s", cs2.memory.read(mapNameAddress+0x4, 32, memprocfs.FLAG_NOCACHE))[0].decode('utf-8', 'ignore')
+            mapname = str(mapname)
+            if 'empty' in mapname:
+                mappp = False
         except:
             pass
 
@@ -147,15 +153,18 @@ class player1:
     def __init__(self, entity_id, EntityAddress):
         self.entity_id = entity_id
         self.EntityAddress = EntityAddress
+        self.Hp = struct.unpack("<I", cs2.memory.read(entity_id + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
+        if self.Hp == 0:
+            return
         self.pX = struct.unpack("<f", cs2.memory.read(entity_id + m_vOldOrigin +0x4, 4, memprocfs.FLAG_NOCACHE))[0]
         self.pY = struct.unpack("<f", cs2.memory.read(entity_id + m_vOldOrigin, 4, memprocfs.FLAG_NOCACHE))[0]
         self.pZ = struct.unpack("<f", cs2.memory.read(entity_id + m_vOldOrigin +0x8, 4, memprocfs.FLAG_NOCACHE))[0]
-        self.Hp = struct.unpack("<I", cs2.memory.read(entity_id + m_iHealth, 4, memprocfs.FLAG_NOCACHE))[0]
         self.team = struct.unpack("<I", cs2.memory.read(entity_id + m_iTeamNum, 4, memprocfs.FLAG_NOCACHE))[0]
         self.EyeAngles = struct.unpack("<fff", cs2.memory.read(entity_id +(m_angEyeAngles +0x4) , 12, memprocfs.FLAG_NOCACHE))
         self.EyeAngles = math.radians(self.EyeAngles[0]+rot_angle)
         self.isdefusing = struct.unpack("<I", cs2.memory.read(entity_id + m_bIsDefusing, 4, memprocfs.FLAG_NOCACHE))[0]
-        self.hasdefuser = struct.unpack("?", cs2.memory.read(EntityAddress + m_bPawnHasDefuser, 1, memprocfs.FLAG_NOCACHE))[0]
+        if isdefusing == 1:
+            self.hasdefuser = struct.unpack("?", cs2.memory.read(EntityAddress + m_bPawnHasDefuser, 1, memprocfs.FLAG_NOCACHE))[0]
         self.color = struct.unpack("<I", cs2.memory.read(EntityAddress + m_iCompTeammateColor, 4, memprocfs.FLAG_NOCACHE))[0]
     def draw(self, screen):
         if mapname in maps_with_split:
@@ -187,6 +196,9 @@ class player1:
             if self.color == 4:
                 pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
                 pygame.draw.circle(screen, (167, 107, 243), (transformed_x, transformed_y), circle_size)
+            else:
+                pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
+                pygame.draw.circle(screen, (255, 255, 255), (transformed_x, transformed_y), circle_size)
         if self.Hp > 0 and self.team != playerTeam:
             pygame.draw.polygon(screen, triangle_color, [(triangle_top_x, triangle_top_y), (triangle_left_x, triangle_left_y), (triangle_right_x, triangle_right_y)])
             pygame.draw.circle(screen, (255, 0, 0), (transformed_x, transformed_y), circle_size)
@@ -251,11 +263,12 @@ while running:
     else:
         print(f'[-] Please, import this map first ({mapname})')
         continue
+    mappp = True
     if mapname in maps_with_split:
         lowerx,lowery,lowerz = getlowermapdata(mapname)
     scale,x,y = getmapdata(mapname)
     map_image = pygame.image.load(f'maps/{mapname}/radar.png')
-    while not 'empty' in get_only_mapname():
+    while mappp:
         try:
             players = []
             for Pawn, EntityAddress in global_entity_list:
