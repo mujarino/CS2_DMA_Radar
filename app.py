d56e3a1b5f2d6803fa0c177f691c152b891b192c
meanwhiletothestars <132231917+meanwhiletothestars@users.noreply.github.com>
2024-03-13 23:28:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -123,9 +123,9 @@ def get_weapon(ptr):
 
 def world_to_minimap(x, y, pos_x, pos_y, scale, map_image, screen, zoom_scale, rotation_angle):
     try:
-        image_x = int((x - pos_x) * map_image.get_width() / (map_image.get_width() * scale * zoom_scale))
-        image_y = int((y - pos_y) * map_image.get_height() / (map_image.get_height() * scale * zoom_scale))
-        center_x, center_y = map_image.get_width() // 2, map_image.get_height() // 2
+        image_x = int((x - pos_x) * screen.get_width() / (map_image.get_width() * scale * zoom_scale))
+        image_y = int((y - pos_y) * screen.get_height() / (map_image.get_height() * scale * zoom_scale))
+        center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
         image_x, image_y = rotate_point((center_x, center_y), (image_x, image_y), rotation_angle)
         return int(image_x), int(image_y)
     except:
