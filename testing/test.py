1c43ff3db9c2e57197786277caeb9c3d3b21eb40
meanwhiletothestars <132231917+meanwhiletothestars@users.noreply.github.com>
2024-03-30 22:23:25 +0300

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -65,7 +65,7 @@ print(f"[+] Finded client base")
 for key, value in offsets["client.dll"].items():
     try:
         playerpawn = struct.unpack("<Q", cs2.memory.read(client_base + value, 8, memprocfs.FLAG_NOCACHE))[0]
-        time = struct.unpack("<I", cs2.memory.read(playerpawn + m_iRoundTime, 4, memprocfs.FLAG_NOCACHE))[0]
+        time = struct.unpack("<I", cs2.memory.read(playerpawn + 3780, 4, memprocfs.FLAG_NOCACHE))[0]
         print(f'Time - {time}, current - {key}')
     except Exception as e:
         print(f'{key} - {e}')
