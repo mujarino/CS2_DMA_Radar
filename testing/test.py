596bd98968d0368a2e2245aad4ab6ea24ec9cb47
meanwhiletothestars <132231917+meanwhiletothestars@users.noreply.github.com>
2024-03-30 23:07:02 +0300

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -82,6 +82,10 @@ client = cs2.module('client.dll')
 client_base = client.base
 print(f"[+] Finded client base")
 
+EntityList = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+EntityList = struct.unpack("<Q", cs2.memory.read(EntityList + 0x10, 8, memprocfs.FLAG_NOCACHE))[0]
+EntityPawnListEntry = struct.unpack("<Q", cs2.memory.read(client_base + dwEntityList, 8, memprocfs.FLAG_NOCACHE))[0]
+
 for i in range(2):
     EntityAddress = struct.unpack("<Q", cs2.memory.read(EntityList + (i + 1) * 0x78, 8, memprocfs.FLAG_NOCACHE))[0]
     Pawn = struct.unpack("<Q", cs2.memory.read(EntityAddress + m_hPlayerPawn, 8, memprocfs.FLAG_NOCACHE))[0]
